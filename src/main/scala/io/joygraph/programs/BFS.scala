87ef9f32921d885e9947980cc8e7ce04b90e8d4b
Sietse T. Au <sietse.au@gmail.com>
2016-03-17 17:27:14 +0100

    added Long.MaxValue for unvisited nodes

--- src/main/scala/io/joygraph/programs/BFS.scala
+++ src/main/scala/io/joygraph/programs/BFS.scala
@@ -3,23 +3,26 @@ package io.joygraph.programs
 import com.typesafe.config.Config
 import io.joygraph.core.program.{NullClass, Vertex, VertexProgram}
 
-class BFS extends VertexProgram[Long,Int, NullClass, Int] {
+object BFS {
+  val UNVISITED : Long = Long.MaxValue
+}
 
+class BFS extends VertexProgram[Long, Long, NullClass, Long] {
   var sourceId : Long = -1
   override def load(conf : Config): Unit = {
     sourceId = conf.getLong("source_id")
   }
 
-  override def run(v: Vertex[Long, Int, NullClass, Int], messages : Iterable[Int], superStep : Int): Boolean = {
+  override def run(v: Vertex[Long, Long, NullClass, Long], messages : Iterable[Long], superStep : Int): Boolean = {
     if (superStep == 0) {
       if (v.id == sourceId) {
         v.value = superStep
         v.sendAll(superStep)
       } else {
-        v.value = -1
+        v.value = BFS.UNVISITED
       }
     } else {
-      if (v.value < 0) {
+      if (v.value == BFS.UNVISITED) {
         v.value = superStep
         v.sendAll(superStep)
       }
