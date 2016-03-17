c4a80be87a123d4b5182d2c4af77fd660b1e6626
Sietse T. Au <sietse.au@gmail.com>
2016-03-17 14:35:48 +0100

    added printing of stacktrace on failure in FutureUtil

--- core/src/main/scala/io/joygraph/core/util/FutureUtil.scala
+++ core/src/main/scala/io/joygraph/core/util/FutureUtil.scala
@@ -10,6 +10,10 @@ object FutureUtil {
   def callbackOnAllComplete[T <: Any, R <: Any](futures : Iterable[Future[T]])(anyFunc : => R)(implicit executor: ExecutionContext): Future[R] = Future {
     val numFutures = futures.size
     val latch = new CountDownLatch(numFutures)
+    futures.foreach(_.recover{
+      case t : Throwable =>
+        t.printStackTrace()
+    })
     futures.foreach(_.foreach(_ => latch.countDown()))
     // todo on fail
     latch.await()
@@ -19,6 +23,10 @@ object FutureUtil {
   def callbackOnAllCompleteWithResults[T <: Any, R <: Any](futures : Iterable[Future[T]])(anyFunc : Iterable[T] => R)(implicit executor: ExecutionContext): Future[R] = Future {
     val numFutures = futures.size
     val latch = new CountDownLatch(numFutures)
+    futures.foreach(_.recover{
+      case t : Throwable =>
+        t.printStackTrace()
+    })
     futures.foreach(_.foreach(_ => latch.countDown()))
     // todo on fail
     println("Waiting is a bitch")
