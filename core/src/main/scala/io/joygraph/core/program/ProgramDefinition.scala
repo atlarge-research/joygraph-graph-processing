f793f59ed5fcd140c4604fc0ff28ae928bf267cb
Sietse T. Au <sietse.au@gmail.com>
2016-03-20 17:56:00 +0100

    inbetween representation of VertexProgram with input messages and output messages.

--- core/src/main/scala/io/joygraph/core/program/ProgramDefinition.scala
+++ core/src/main/scala/io/joygraph/core/program/ProgramDefinition.scala
@@ -8,11 +8,11 @@ object ProgramDefinition {
   def create[INPUTFORMAT, I : ClassTag,V : ClassTag,E : ClassTag,M : ClassTag]
   (inputParser : INPUTFORMAT => (I, I, E),
    outputWriter : (Vertex[I,V,E], OutputStream) => Any,
-   program : Class[_ <: VertexProgramLike[I,V,E,M]]): ProgramDefinition[INPUTFORMAT, I,V,E,M] = new ProgramDefinition[INPUTFORMAT, I,V,E,M](inputParser, outputWriter, program)
+   program : Class[_ <: VertexProgramLike[I,V,E,M,M]]): ProgramDefinition[INPUTFORMAT, I,V,E,M] = new ProgramDefinition[INPUTFORMAT, I,V,E,M](inputParser, outputWriter, program)
 
 }
 
-case class ProgramDefinition[INPUTFORMAT, I : ClassTag,V : ClassTag,E : ClassTag,M : ClassTag](val inputParser : INPUTFORMAT => (I, I, E), val outputWriter : (Vertex[I,V,E], OutputStream) => Any, val program : Class[_ <: VertexProgramLike[I,V,E,M]]) {
+case class ProgramDefinition[INPUTFORMAT, I : ClassTag,V : ClassTag,E : ClassTag,M : ClassTag](val inputParser : INPUTFORMAT => (I, I, E), val outputWriter : (Vertex[I,V,E], OutputStream) => Any, val program : Class[_ <: VertexProgramLike[I,V,E,M,M]]) {
   val clazzI : Class[I] = classTag[I].runtimeClass.asInstanceOf[Class[I]]
   val clazzV : Class[V] = classTag[V].runtimeClass.asInstanceOf[Class[V]]
   val clazzE : Class[E] = classTag[E].runtimeClass.asInstanceOf[Class[E]]
--- core/src/main/scala/io/joygraph/core/program/VertexProgram.scala
+++ core/src/main/scala/io/joygraph/core/program/VertexProgram.scala
@@ -8,12 +8,10 @@ object VertexProgram {
   }
 }
 
-trait VertexProgram[I,V,E,M] extends VertexProgramLike[I,V,E,M] {
+trait VertexProgram[I,V,E,M] extends VertexProgramLike[I,V,E,M,M] {
 
   private[this] var _numVertices : Long = _
   private[this] var _numEdges : Long = _
-  private[this] var _sendFunc : (M,I) => Any = _
-  private[this] var _sendAllFunc : (Vertex[I,V,E],M) => Any = _
 
   /**
     * Load parameters from vertex program
@@ -21,23 +19,9 @@ trait VertexProgram[I,V,E,M] extends VertexProgramLike[I,V,E,M] {
     * @param conf
     */
   def load(conf : Config)
-
-  final def messageSenders(sendFunc : (M, I) => Any , sendAllFunc : (Vertex[I,V,E],M) => Any) = {
-    _sendFunc = sendFunc
-    _sendAllFunc = sendAllFunc
-  }
-
   def totalNumVertices(numVertices : Long) = _numVertices = numVertices
   def totalNumVertices = _numVertices
   def totalNumEdges(numEdges : Long) = _numEdges = numEdges
   def totalNumEdges = _numEdges
-
-  /**
-    * @param v
-    * @return true if halting, false if not halting
-    */
-  def run(v: Vertex[I, V, E], messages: Iterable[M], superStep: Int): Boolean
-  def send(m: M, i: I): Unit = _sendFunc(m, i)
-  def sendAll(v : Vertex[I,V,E], m: M): Unit = _sendAllFunc(v, m)
 
 }
