b03cb0a7ad3ccb769269549a5fae6a55c9486a20
Sietse T. Au <sietse.au@gmail.com>
2016-03-24 01:33:27 +0100

    combinable test

--- src/main/scala/io/joygraph/programs/BFSCombinable.scala
+++ src/main/scala/io/joygraph/programs/BFSCombinable.scala
@@ -1,7 +1,39 @@
 package io.joygraph.programs
 
-import io.joygraph.core.program.Combinable
+import com.typesafe.config.Config
+import io.joygraph.core.program._
 
-class BFSCombinable extends BFS with Combinable[Long]{
-  override def combine(m1: Long, m2: Long): Long = math.min(m1, m2)
+class BFSCombinable extends NewVertexProgram[Long, Long, NullClass] {
+  var sourceId : Long = -1
+  override def load(conf : Config): Unit = {
+    sourceId = conf.getLong(BFS.SOURCE_ID_CONF_KEY)
+  }
+
+  override def run(): PartialFunction[Int, SuperStepFunction[Long, Long, NullClass,_, _]] = {
+    case superStep @ 0 =>
+      new SuperStepFunction(this, classOf[Long], classOf[Long]) {
+        override def func: (Vertex[Long, Long, NullClass], Iterable[Long]) => Boolean =
+          (v, m) => {
+            if (v.id == sourceId) {
+              sendAll(v, superStep)
+            } else {
+              v.value = BFS.UNVISITED
+            }
+            true
+          }
+      }
+    case superStep @ _ =>
+      new SuperStepFunction(this, classOf[Long], classOf[Long]) with Combinable[Long] {
+        override def func: (Vertex[Long, Long, NullClass], Iterable[Long]) => Boolean =
+          (v, m) => {
+            if (v.value == BFS.UNVISITED) {
+              v.value = superStep
+              sendAll(v, superStep)
+            }
+            true
+          }
+
+        override def combine(m1: Long, m2: Long): Long = math.min(m1, m2)
+      }
+  }
 }
