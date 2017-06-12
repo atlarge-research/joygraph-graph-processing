df4cf8f9575382a10dca29665047ff003522985a
Sietse T. Au <sietse.au@gmail.com>
2017-06-12 08:03:14 +0200

    added mean std to each resource per step

--- analysis/src/main/scala/io/joygraph/analysis/algorithm/AlgorithmMetric.scala
+++ analysis/src/main/scala/io/joygraph/analysis/algorithm/AlgorithmMetric.scala
@@ -2,18 +2,18 @@ package io.joygraph.analysis.algorithm
 import akka.cluster.metrics.Metric
 import io.joygraph.analysis.ElasticPolicyReader
 import io.joygraph.core.actor.metrics.{NetworkMetrics, OffHeapMemoryMetrics}
+import org.apache.commons.math3.stat.descriptive.moment.{Mean, StandardDeviation}
 
 import scala.collection.JavaConversions._
 import scala.collection.immutable
-import scala.collection.mutable.ArrayBuffer
 
 case class AlgorithmMetric
 (activeVerticesPerStep : immutable.Seq[Long],
- activeVerticesPerStepPerWorker : immutable.Seq[Iterable[(Int, Option[Long])]],
- offHeapMemoryPerStepPerWorker : immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]],
- bytesSentPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]],
- bytesReceivedPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]],
- wallClockPerStepPerWorker : immutable.Seq[Iterable[(Int, Long)]]
+ wallClockPerStepPerWorker : immutable.Seq[Iterable[(Int, Long)]],
+ activeVerticesPerStepPerWorker : immutable.Seq[Iterable[(Int, Long)]],
+ offHeapMemoryPerStepPerWorker : immutable.IndexedSeq[Iterable[(Int, Statistics)]],
+ bytesReceivedPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, Statistics)]],
+ bytesSentPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, Statistics)]]
 ) {
 
 }
@@ -32,8 +32,11 @@ object AlgorithmMetric {
     }
   }
 
-  private def extractGeneralMetricsPerStepPerWorker[T]
-  (elasticPolicyReader: ElasticPolicyReader, transformer : Metric => Option[T]): immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[T]])]] = {
+  private def extractGeneralMetricsPerStepPerWorker[T,A]
+  (elasticPolicyReader: ElasticPolicyReader,
+   transformer : Metric => Option[T],
+   aggregator : Iterable[T] => A
+  ): immutable.IndexedSeq[Iterable[(Int, A)]] = {
     for (i <- 0 until elasticPolicyReader.totalNumberOfSteps())
       yield {
         for (workerId <- elasticPolicyReader.workersForStep(i))
@@ -47,55 +50,58 @@ object AlgorithmMetric {
       }
   }
 
+  private def extractGeneralMetricsPerStepPerWorkerAggregateLong
+  (elasticPolicyReader: ElasticPolicyReader,
+   transformer : Metric => Option[Long]
+  ): immutable.IndexedSeq[Iterable[(Int, Statistics)]] = {
+    extractGeneralMetricsPerStepPerWorker[Long, Statistics](elasticPolicyReader,
+      transformer,
+      collection => {
+        val rawArray = collection.map(_.toDouble).toArray
+        val std = new StandardDeviation(true)
+        val mean = new Mean()
+        Statistics(
+          std.evaluate(),
+          mean.evaluate(rawArray),
+          rawArray.length
+        )
+      }
+    )
+  }
+
+
+  private def aggregated[T]
+  (
+    rawPerWorker: Iterable[(Int, Iterable[T])],
+    transformer : Iterable[T] => Statistics
+  ) : Iterable[(Int, Statistics)]= {
+    rawPerWorker.map{
+      case (workerId, collection) =>
+        workerId -> transformer(collection)
+    }
+  }
+
   def calculate(policyMetricsReader : ElasticPolicyReader): AlgorithmMetric = {
     val activeVerticesPerStep : immutable.Seq[Long] = {
       for (i <- 0 until policyMetricsReader.totalNumberOfSteps())
         yield policyMetricsReader.activeVerticesSumOf(i)
     }
 
-    val activeVerticesPerStepPerWorker: immutable.Seq[Iterable[(Int, Option[Long])]] = {
+    val activeVerticesPerStepPerWorker: immutable.Seq[Iterable[(Int, Long)]] = {
       for (i <- 0 until policyMetricsReader.totalNumberOfSteps())
         yield {
           for (workerId <- policyMetricsReader.workersForStep(i))
             yield {
-              workerId -> policyMetricsReader.activeVerticesOf(i, workerId)
+              workerId -> {
+                policyMetricsReader.activeVerticesOf(i, workerId) match {
+                  case Some(x) =>
+                    x
+                  case None =>
+                    0
+                }
+              }
             }
         }
-    }
-    val averageOffHeapMemoryPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]] = {
-        extractGeneralMetricsPerStepPerWorker(policyMetricsReader,
-          metric =>
-            metric.name match {
-            case OffHeapMemoryMetrics.OffHeapMemoryUsed =>
-              Some[Long](metric.value.longValue())
-            case _ =>
-              None
-          }
-        )
-    }
-
-    val bytesSentPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]] = {
-      extractGeneralMetricsPerStepPerWorker(policyMetricsReader,
-        metric =>
-          metric.name match {
-            case NetworkMetrics.BytesSent =>
-              Some[Long](metric.value.longValue())
-            case _ =>
-              None
-          }
-      )
-    }
-
-    val bytesReceivedPerStepPerWorker : immutable.IndexedSeq[Iterable[(Int, ArrayBuffer[Iterable[Long]])]] = {
-      extractGeneralMetricsPerStepPerWorker(policyMetricsReader,
-        metric =>
-          metric.name match {
-            case NetworkMetrics.BytesReceived =>
-              Some[Long](metric.value.longValue())
-            case _ =>
-              None
-          }
-      )
     }
 
     val wallClockPerStepPerWorker : immutable.Seq[Iterable[(Int, Long)]] = {
@@ -108,13 +114,49 @@ object AlgorithmMetric {
         }
     }
 
+    val averageOffHeapMemoryPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, Statistics)]] = {
+      extractGeneralMetricsPerStepPerWorkerAggregateLong(policyMetricsReader,
+          metric =>
+            metric.name match {
+            case OffHeapMemoryMetrics.OffHeapMemoryUsed =>
+              Some[Long](metric.value.longValue())
+            case _ =>
+              None
+          }
+        )
+    }
+
+    val bytesSentPerStepPerWorker: immutable.IndexedSeq[Iterable[(Int, Statistics)]] = {
+      extractGeneralMetricsPerStepPerWorkerAggregateLong(policyMetricsReader,
+        metric =>
+          metric.name match {
+            case NetworkMetrics.BytesSent =>
+              Some[Long](metric.value.longValue())
+            case _ =>
+              None
+          }
+      )
+    }
+
+    val bytesReceivedPerStepPerWorker : immutable.IndexedSeq[Iterable[(Int, Statistics)]] = {
+      extractGeneralMetricsPerStepPerWorkerAggregateLong(policyMetricsReader,
+        metric =>
+          metric.name match {
+            case NetworkMetrics.BytesReceived =>
+              Some[Long](metric.value.longValue())
+            case _ =>
+              None
+          }
+      )
+    }
+
     AlgorithmMetric(
       activeVerticesPerStep,
+      wallClockPerStepPerWorker,
       activeVerticesPerStepPerWorker,
       averageOffHeapMemoryPerStepPerWorker,
-      bytesSentPerStepPerWorker,
       bytesReceivedPerStepPerWorker,
-      wallClockPerStepPerWorker
+      bytesSentPerStepPerWorker
     )
   }
 }
